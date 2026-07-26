506c9fd065f836c793952ff6f188ed91641331a9
Kevin Russel <kzone159@gmail.com>
2026-07-25 22:43:27 -0500

    first chunk done

--- scanner.py
+++ scanner.py
@@ -6,10 +6,10 @@ port_range_high = 8080
 class Packet:
 
     def __init__(self):
+        # -- FIRST CHUNK ## 
         self.version = 0x4
         self.IHL = 0x5
         self.v_ihl = (self.version << 4) | self.IHL
-
         self.tos = 0x00
         self.total_length = 0x33 
         print(self.v_ihl)
