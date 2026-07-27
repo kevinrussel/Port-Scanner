1bc5e429d2682f4190fc5dc646cc3458e064e66a
Kevin Russel <kzone159@gmail.com>
2026-07-26 20:35:47 -0500

    fixing dest

--- scanner.py
+++ scanner.py
@@ -40,7 +40,7 @@ class Packet:
         # -- TCP HEADER --#
         ## First Chunk
         self.src_port = 0x1F90
-        self.dest_port = 0x1F90
+        self.dest_port = 0x1F91
 
 
 packet = Packet("127.0.0.1", "127.0.0.1", 8080)
