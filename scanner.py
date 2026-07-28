1bfa18e160013acbbafec0dedf6d42754fab147b
Kevin Russel <kzone159@gmail.com>
2026-07-27 23:07:40 -0500

    generate packet isn't done yet

--- scanner.py
+++ scanner.py
@@ -71,6 +71,6 @@ class Packet:
 
     def generate_packet(self):
         # IP Header + checksum
-        final_ip_header = pack("!BBHHH",self.v_ihl,self.tos,self.total_length,self.identification,self.f_fragment_offset)
+        final_ip_header = pack("!BBHHHBBH",self.v_ihl,self.tos,self.total_length,self.identification,self.f_fragment_offset,self.ttl,self.protocol,self.create_tmp_ip_header(),)
 
 packet = Packet("127.0.0.1", "127.0.0.1", 8081)
