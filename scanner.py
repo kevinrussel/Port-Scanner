3b0f2ae671489b8e2d9de633ba1fdf492ac41d07
Kevin Russel <kzone159@gmail.com>
2026-07-28 22:05:31 -0500

    adding tcp

--- scanner.py
+++ scanner.py
@@ -86,9 +86,13 @@ class Packet:
         tmp_ip_header = pack("!BBHHHBBH4s4s", self.v_ihl,self.tos,self.total_length,self.identification,self.f_fragment_offset,self.ttl,self.protocol,self.checksum,self.src_addr,self.dest_addr)
         return tmp_ip_header
 
+    def generate_tmp_tcp_header(self):
+        
+
     
     def generate_packet(self):
         # IP Header + checksum
-        final_ip_header = pack("!BBHHHBH4s4s",self.v_ihl,self.tos,self.total_length,self.identification,self.f_fragment_offset,self.ttl,self.protocol,self.calc_checksum(self.generate_tmp_ip_header()),self.src_addr,self.dest_addr)
+        final_ip_header = pack("!BBHHHBBH4s4s",self.v_ihl,self.tos,self.total_length,self.identification,self.f_fragment_offset,self.ttl,self.protocol,self.calc_checksum(self.generate_tmp_ip_header()),self.src_addr,self.dest_addr)
 
+        final_tcp_header = pack("!")
 packet = Packet("127.0.0.1", "127.0.0.1", 8081)
