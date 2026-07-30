9f0be18e64ef73c532294325304d9e704ea79f82
Kevin Russel <kzone159@gmail.com>
2026-07-29 19:59:40 -0500

    removing old code

--- scanner.py
+++ scanner.py
@@ -123,26 +123,3 @@ class Packet:
         return data
 
     
-import struct
-def check_if_open(port, response):
-    
-
-    ip = response[:20]
-    tcp = response[20:40]
-    src_port, dst_port, seq, ack, flags, *_ = struct.unpack(
-    "!HHLLHHHH", tcp
-)
-    
-
-    if (flags & 0x1FF == 0x12):
-        print("Port "+str(port)+" is: open")
-    else:
-        print("Port "+str(port)+" is: closed")
-    
-for value in range(8081,8090):
-    packet = Packet("127.0.0.1", "127.0.0.1", value)
-    packet.generate_packet()
-
-
-    result = packet.send_packet()
-    check_if_open(value,result)
