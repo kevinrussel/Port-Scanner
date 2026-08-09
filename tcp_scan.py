54c470927610182e9e859312ba5089ca7cbb6db0
Kevin Russel <kzone159@gmail.com>
2026-08-09 12:29:10 -0500

    adding code

--- tcp_scan.py
+++ tcp_scan.py
@@ -111,9 +111,7 @@ class Packet:
     def generate_fin_packet(self):
         # IP Header + checksum
         self.data_offset_res_flags = self.calculate_tcp_flags(tcp_offset=self.tcp_offset,reserved=self.reserved,ns=self.ns,cwr=self.cwr,ece=self.ece,urg=self.urg,ack=self.ack,psh=self.psh,rst=self.rst,syn=self.syn,fin=0x1)
-
         final_ip_header = pack("!BBHHHBBH4s4s",self.v_ihl,self.tos,self.total_length,self.identification,self.f_fragment_offset,self.ttl,self.protocol,self.calc_checksum(self.generate_tmp_ip_header()),self.src_addr,self.dest_addr)
-
         tmp_tcp_header = self.generate_tmp_tcp_header()
         pseudo_header = pack("!4s4sBBH",self.src_addr,self.dest_addr,0x0,self.protocol,len(tmp_tcp_header))
         psh = pseudo_header + tmp_tcp_header
