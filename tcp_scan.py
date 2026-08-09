cee7c56078e9de9561b88df19401b2fb058d808f
Kevin Russel <kzone159@gmail.com>
2026-08-09 11:51:58 -0500

    changing flag code

--- tcp_scan.py
+++ tcp_scan.py
@@ -47,18 +47,20 @@ class Packet:
         self.ack_num = 0x0
 
         ## Third chunk
-        self.tcp_offset = 0x5
-        self.reserved = 0x0
-        self.ns = 0x0
-        self.cwr = 0x0
-        self.ece = 0x0
-        self.urg = 0x0
-        self.ack = 0x0
-        self.psh = 0x0
-        self.rst = 0x0
-        self.syn = 0x1
-        self.fin = 0x0
-        self.data_offset_res_flags = (self.tcp_offset << 12) | (self.reserved << 9) | (self.ns << 8) | (self.cwr << 7)|(self.ece << 6) | (self.urg << 5) | (self.ack << 4) | (self.psh << 3) | (self.rst << 2) | (self.syn << 1) | (self.fin)
+        # self.tcp_offset = 0x5
+        # self.reserved = 0x0
+        # self.ns = 0x0
+        # self.cwr = 0x0
+        # self.ece = 0x0
+        # self.urg = 0x0
+        # self.ack = 0x0
+        # self.psh = 0x0
+        # self.rst = 0x0
+        # self.syn = 0x1
+        # self.fin = 0x0
+        self.data_offset_res_flags = 0x0
+        ##TODO: Get rid of this.
+        (self.tcp_offset << 12) | (self.reserved << 9) | (self.ns << 8) | (self.cwr << 7)|(self.ece << 6) | (self.urg << 5) | (self.ack << 4) | (self.psh << 3) | (self.rst << 2) | (self.syn << 1) | (self.fin)
         self.window_size = 0x7110
 
         ## FOURTH CHUNK:
@@ -70,7 +72,7 @@ class Packet:
         self.packet = b""
 
     def calculate_tcp_flags(self,tcp_offset,reserved,ns,cwr,ece,urg,ack,psh,rst,syn,fin):
-        pass
+        return (self.tcp_offset << 12) | (self.reserved << 9) | (self.ns << 8) | (self.cwr << 7)|(self.ece << 6) | (self.urg << 5) | (self.ack << 4) | (self.psh << 3) | (self.rst << 2) | (self.syn << 1) | (self.fin)
 
     def calc_checksum(self, msg):
         s = 0
